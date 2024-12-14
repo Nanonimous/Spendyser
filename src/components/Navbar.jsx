fc361e1b92b0e5a8a43342e3fe0fcf766f49e7a3
Nanonimous <COCNIVAAS@GMAIL.COM>
2024-12-14 22:50:49 +0530

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
@@ -7,6 +7,22 @@ export const Navbar = () => {
     const toggleMenu = () => {
       setIsMenuOpen(!isMenuOpen);
     };
+    useEffect(() => {
+      const handleResize = () => {
+          // If the screen width is greater than 768px, close the menu
+          if (window.innerWidth > 768) {
+              setIsMenuOpen(false);
+          }
+      };
+
+      // Attach the event listener on mount
+      window.addEventListener('resize', handleResize);
+
+      // Cleanup the event listener on unmount
+      return () => {
+          window.removeEventListener('resize', handleResize);
+      };
+    }, []);
     return (
       <nav>
         <div className="container-navbar">
@@ -14,15 +30,14 @@ export const Navbar = () => {
               <a href="#" className="logoname">Spendyser</a>
 
               <div className="hamburger" onClick={toggleMenu}>
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
+                <img src='/images/menu1.png' className={!isMenuOpen ? 'burger-logo' : 'burger-logo-active'} alt="" />
               </div>
 
-              <ul className="nav-ul">
+              <ul className={`nav-ul ${isMenuOpen ? 'nav-active' : 'nav-deactive'}`}>
                 <li><a href="#">ABOUT</a></li>
                 <li><a href="#">FEATURES</a></li>
                 <li><a href="#">CONTACT</a></li>
+                {isMenuOpen ? <li><div className='getstarted-ham'><a href="#">GET STARTED</a></div></li> : ''}
               </ul>
 
               <div className="getstarted">
