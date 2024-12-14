21f8f8db5ad442fbec7aad2e0001102d6e6cb55f
Nanonimous <COCNIVAAS@GMAIL.COM>
2024-12-14 12:51:01 +0530

    navbar commit 1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+    return (
+      <nav>
+        <div className="container-navbar">
+          <div className="main-navbar">
+              <a href="#" className="logoname">Spendyser</a>
+              <ul className="nav-ul">
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">FEATURES</a></li>
+                <li><a href="#">CONTACT</a></li>
+              </ul>
+              <div className="getstarted">
+                <a href="">GET STARTED</a>
+              </div>
+          </div>
+        </div>
+      </nav>
+    )
+}
